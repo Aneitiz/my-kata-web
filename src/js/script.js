4ea1ea1b0256be58a6fd63d1809d709010bf51dd
Aneitiz <89402121+Aneitiz@users.noreply.github.com>
2022-11-20 19:52:10 +0700

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,55 @@
-!'use strict'
+!"use strict";
+const slider = document.querySelector(".swiper");
+let sliderShowBtn = document.querySelector(".card__show-more");
+let sliderBtnText = document.querySelector(".card__show-more-text");
+let sliderBtnImage = document.querySelector(".slider__show-image");
+let cardHiddenItem = document.querySelector(".card--hidden");
+let toggler;
+let mySwiper;
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
+let sliderToggler = function (elements) {
+  toggler = sliderShowBtn.addEventListener("click", () => {
+    elements.classList.toggle("card");
+	if(elements.classList.contains("card")){
+		sliderBtnText.innerHTML = "скрыть";
+		sliderBtnImage.src = "src/assets/img/arrows-up.svg"
+	  }else if(elements.classList.contains("card--hidden")){
+		sliderBtnText.innerHTML = "Показать всё";
+		sliderBtnImage.src = "src/assets/img/arrows-dawn.svg"
+	  }
+  });
+};
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+function mobileSlider() {
+  if (window.innerWidth < 768 && slider.dataset.mobile == "false") {
+    mySwiper = new Swiper(".swiper", {
+      grabCursor: true,
+      loop: true,
+      slidesPerView: "1.3",
+      spaceBetween: 16,
+      pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true,
+      },
+    });
+    slider.dataset.mobile = "true";
+  }
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  if (window.innerWidth > 767) {
+    slider.dataset.mobile = "false";
+    if (slider.classList.contains("swiper-initialized")) {
+      mySwiper.destroy();
+    }
+  }
+}
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+mobileSlider();
+
+window.addEventListener("resize", () => {
+  mobileSlider();
 });
+
+sliderToggler(cardHiddenItem);
+
+
