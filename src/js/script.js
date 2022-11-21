8bbf465e6f5299603f11b3f1fef548040ffed5fb
Aneitiz <89402121+Aneitiz@users.noreply.github.com>
2022-11-21 21:06:05 +0700

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,54 @@
 !"use strict";
 const slider = document.querySelector(".swiper");
-let sliderShowBtn = document.querySelector(".card__show-more");
-let sliderBtnText = document.querySelector(".card__show-more-text");
-let sliderBtnImage = document.querySelector(".slider__show-image");
-let cardHiddenItem = document.querySelector(".card--hidden");
+const sliderShowBtn = document.querySelector(".card__show-more");
+const sliderBtnText = document.querySelector(".card__show-more-text");
+const sliderBtnImage = document.querySelector(".slider__show-image");
+const cardHiddenItem = document.querySelector(".card--hidden");
+const mediaQuery = window.matchMedia("(min-width: 768px)");
 let toggler;
 let mySwiper;
 
-let sliderToggler = function (elements) {
-  toggler = sliderShowBtn.addEventListener("click", () => {
-    elements.classList.toggle("card");
-	if(elements.classList.contains("card")){
-		sliderBtnText.innerHTML = "скрыть";
-		sliderBtnImage.src = "src/assets/img/arrows-up.svg"
-	  }else if(elements.classList.contains("card--hidden")){
-		sliderBtnText.innerHTML = "Показать всё";
-		sliderBtnImage.src = "src/assets/img/arrows-dawn.svg"
-	  }
-  });
-};
-
-function mobileSlider() {
-  if (window.innerWidth < 768 && slider.dataset.mobile == "false") {
-    mySwiper = new Swiper(".swiper", {
-      grabCursor: true,
-      loop: true,
-      slidesPerView: "1.3",
-      spaceBetween: 16,
-      pagination: {
-        el: ".swiper-pagination",
-        type: "bullets",
-        clickable: true,
-      },
+document.addEventListener("DOMContentLoaded", () => {
+  let sliderToggler = function (elements) {
+    toggler = sliderShowBtn.addEventListener("click", () => {
+      elements.classList.toggle("card");
+      if (elements.classList.contains("card")) {
+        sliderBtnText.innerHTML = "скрыть";
+        sliderBtnImage.src = "src/assets/img/arrows-up.svg";
+      } else if (elements.classList.contains("card--hidden")) {
+        sliderBtnText.innerHTML = "Показать всё";
+        sliderBtnImage.src = "src/assets/img/arrows-dawn.svg";
+      }
     });
-    slider.dataset.mobile = "true";
-  }
+  };
 
-  if (window.innerWidth > 767) {
-    slider.dataset.mobile = "false";
-    if (slider.classList.contains("swiper-initialized")) {
-      mySwiper.destroy();
+  function mobileSlider() {
+    if (mediaQuery.matches === false) {
+      mySwiper = new Swiper(".swiper", {
+        grabCursor: true,
+        loop: true,
+        slidesPerView: "1.3",
+        spaceBetween: 16,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "bullets",
+          clickable: true,
+        },
+      });
+      slider.dataset.mobile = true;
+      mySwiper.update();
+    }
+    if (mediaQuery.matches) {
+      slider.dataset.mobile = "false";
+      if (slider.classList.contains("swiper-initialized")) {
+        slider.dataset.mobile = false;
+        mySwiper.update();
+        mySwiper.destroy();
+      }
     }
   }
-}
 
-mobileSlider();
-
-window.addEventListener("resize", () => {
   mobileSlider();
+
+  sliderToggler(cardHiddenItem);
 });
-
-sliderToggler(cardHiddenItem);
-
-
